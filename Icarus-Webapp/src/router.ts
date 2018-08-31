d5d10e5de36ce7203df2f73e18309ef0fbff4733
Sam Crane <samcrane8@gmail.com>
2018-08-31 09:39:30 -0400

    finally got it working somewhat
    - now our system is solid
    - we can begin migrating towards this vue-cli 3.0 build.
    - still some issues with the video and def need to update mixins to work with the new OAuth and everything.
    - Otherwise things are looking good.

--- Icarus-Webapp/src/router.ts
+++ Icarus-Webapp/src/router.ts
@@ -1,23 +1,28 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+
+const routerOptions = [
+  { path: '/', component: 'LandingPage' },
+  { path: '/login', component: 'Login'},
+  { path: '/map', component: 'MapPage' },
+  { path: '/newflight', component: 'NewFlightsPage' },
+  { path: '/settings', component: 'Settings'},
+  { path: '/homepage', component: 'HomePage'},
+  { path: '/drones', component: 'Drones'},
+  { path: '/flights', component: 'FlightsPage'},
+  { path: '/education', component: 'EducationPage'}
+]
+
+const routes = routerOptions.map(route => {
+  return {
+    path: route.path,
+    component: () => import(`@/views/${route.component}.vue`)
+  }
+})
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
-  ]
+  mode: 'history',
+  routes
 })
